6f3408de0f666781c18dbd6b1dc313dbb8ade3a3
Abhishek <40629087+abhishekpanwarrr@users.noreply.github.com>
2023-09-21 16:07:17 +0530

    backend login and nav completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,12 @@ import { getServerSession } from 'next-auth'
 import Link from 'next/link'
 import { Icons } from './Icons'
 import { buttonVariants } from './ui/Button'
-// import { UserAccountNav } from './UserAccountNav'
+import { getAuthSession } from '@/lib/auth'
+import { UserAccountNav } from './UserAccountNav'
 // import SearchBar from './SearchBar'
 
-const Navbar = () => {
-  // const session = getServerSession(authOptions)
+const Navbar = async() => {
+  const session =  await getAuthSession();
   return (
     <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
       <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
@@ -21,13 +22,14 @@ const Navbar = () => {
         {/* <SearchBar /> */}
 
         {/* actions */}
-        {/* {session?.user ? (
+        {session?.user ? (
           <UserAccountNav user={session.user} />
+          // <p>You'r logged in</p>
         ) : (
-        )} */}
         <Link href='/sign-in' className={buttonVariants()}>
           Sign In
         </Link>
+        )}
       </div>
     </div>
   )
